7063b56e77365706ffbb056810ebd52aa57301bb
pupsiksasiki <c8221057@gmail.com>
2025-11-15 15:01:23 +0200

    Implement anti-AFK and enhance auto-clicker logic
    был дописан и чуть улучшен код также добавлена функция Анти афк и авто-зажим

--- clicker_logic.py
+++ clicker_logic.py
@@ -8,12 +8,20 @@ import win32api
 import win32con
 
 avtoclicker = False
+anti = False
 clicks = None
+chechszajim = False
+zajim = False
 
+
+
+
+
+
+# настроики включения авто кликеров
 def auto_clicker():
     global avtoclicker
     global clicks
-    #тута получение кол кликов
     try:
         if gui.radio2.isChecked():
             colicestva = max(int(gui.stopcik1.text()), 1)
@@ -21,7 +29,7 @@ def auto_clicker():
             colicestva = -1
     except:
         colicestva = -1
-    #тута получение интервала
+ 
     try:
         ms = int(gui.muzic_ms.text()) if gui.muzic_ms.text() != "" else 0
     except:
@@ -49,16 +57,17 @@ def auto_clicker():
     kps_interval = 1/kps  
 
     clicks = 0
-    #тута  кароче получение кнопки мыши
+    
     try:
         button = "right" if gui.knopka1.currentText() == "Правая" else "left"
     except:
         button = "right"
-    # тут думаю понтно
+    
     while avtoclicker and (clicks < colicestva or colicestva == -1):
         click_type = gui.knopka2.currentText()
 
         print(interval, clicks)
+        print(avtoclicker,zajim)
 
         if click_type == "Одиночный":
             pyautogui.click(button=button)
@@ -87,17 +96,100 @@ def auto_clicker():
 
     avtoclicker = False
 
+
+def antiafkdef():
+    global anti
+    print("LOg anti-afk succees enabled")
+    print(anti)
+    while anti:
+        time.sleep(50) 
+        win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, 110, 10, 0, 0)
+        
+        time.sleep(0.1)
+        win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, -110, -10, 0, 0)
+
+
+
+# Проверка включения авто кликера =========================================
 def check():
-    global avtoclicker, click_thread
+    global avtoclicker, click_thread, anti, zajim
     if avtoclicker:
         avtoclicker = False
+        anti = False
+        print("[Log] Выключение кликов")
     else:
+        zajim = False
         avtoclicker = True
         click_thread = threading.Thread(target=auto_clicker, daemon=True)
         click_thread.start()
+        print("[Log] Включение кликов")
+        #========================================================
+
+        # anti afk ======================
+        print("[Log] проверка включения анти афк")
+        anti = gui.antiafk.isChecked()
+        if anti:
+            anti = True
+            threading.Thread(target=antiafkdef, daemon=True).start()
+            print("[Log] anti afk включен")
+        else:
+            anti = False
+            print("[Log] anti-afk выключен")
+        #anti-afk ==================================
+
+
+
+
+
+
+
+
+
+
+# Режим авто-зажима ===========================================
+
+def zajim_click():
+    global zajim
+    while zajim:
+        pyautogui.mouseDown(button="left")
+        
+def check2():
+    global avtoclicker, zajim
+    print("[Log] Проверка для режима авто-зажима активирирована")
+
+    
+    if gui.sajim.isChecked():
+        
+        if not zajim:
+            zajim = True
+            avtoclicker = False
+            pyautogui.mouseUp(button="left")
+            threading.Thread(target=zajim_click, daemon=True).start()
+            print("[Log] авто-зажим активирован")
+        else:
+            zajim = False
+            pyautogui.mouseUp(button="left")
+            print("[Log] Авто-зажим был выключен")
+    else:
+        zajim = False
+        pyautogui.mouseUp(button='left')
+        print("[Log] Авто зажим не был включен в настроиках")
+
+
+# ==========================================================================
+
+
+
+
+# горячие клавиши для запуска
 
 def f6():
     keyboard.add_hotkey("f6", check)
     keyboard.wait("f6")
+def f7():
+    keyboard.add_hotkey("f7", check2)
+    keyboard.wait("f7")
+
 
 threading.Thread(target=f6, daemon=True).start()
+threading.Thread(target=f7, daemon=True).start()
